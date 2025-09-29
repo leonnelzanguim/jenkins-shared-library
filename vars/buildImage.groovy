ecee04bd2fda95881e4cc98e30037e4e4f2e2b43
leonnel Zanguim <58272671+leonnelzanguim@users.noreply.github.com>
2025-09-29 16:14:09 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo 'testing the application...'
   withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t 10012975/demo-java-maven-app:2.0 -f java-maven-app/Dockerfile java-maven-app/'
+    sh "docker build -t $imageName -f java-maven-app/Dockerfile java-maven-app/"
     sh 'echo $PASS | docker login -u $USER --password-stdin'
-    sh 'docker push 10012975/demo-java-maven-app:2.0'
+    sh "docker push $imageName"
   }
 }
