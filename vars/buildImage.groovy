72419df0ad70ced7fe2b2a2ed972bf44e73eca5a
leonnel Zanguim <58272671+leonnelzanguim@users.noreply.github.com>
2025-09-29 15:11:22 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,10 @@
 #!/user/bin/env groovy
 
-import com.example.Docker
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call() {
+    echo 'testing the application...'
+  withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    sh 'docker build -t 10012975/demo-java-maven-app:2.0 -f java-maven-app/Dockerfile java-maven-app/'
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push 10012975/demo-java-maven-app:2.0'
+  }
 }
