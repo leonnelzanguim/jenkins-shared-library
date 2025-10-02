c52257dad05a76de299dc22003afead818d2169a
leonnel Zanguim <58272671+leonnelzanguim@users.noreply.github.com>
2025-10-02 07:42:21 +0200

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -2,5 +2,6 @@
 
 def call() {
   echo "building the application for $GIT_BRANCH"
-  sh 'mvn -f java-maven-app/pom.xml package'
+  // sh 'mvn -f java-maven-app/pom.xml package'
+  sh 'mvn clean package'
 }
